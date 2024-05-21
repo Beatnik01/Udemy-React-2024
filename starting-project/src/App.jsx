b2248ff6c14b12d14e5f80185d132fe80e0a8db4
Gihoon Lim <knv429@naver.com>
2024-05-21 13:18:38 +0900

    #Props(속성)으로 컴포넌트 재사용

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,4 +1,8 @@
 import atomImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import jsxImg from "./assets/jsx-ui.png";
+import propsImg from "./assets/config.png";
+import stateImg from "./assets/state-mgmt.png";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -18,12 +22,30 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title="Components" description="The core UI building block" image={componentsImg} />
+            <CoreConcept title="JSX" description="The core UI building block" image={jsxImg} />
+            <CoreConcept title="Props" description="The core UI building block" image={propsImg} />
+            <CoreConcept title="State" description="The core UI building block" image={stateImg} />
+          </ul>
+        </section>
       </main>
     </div>
   );
