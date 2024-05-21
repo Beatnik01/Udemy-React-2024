672b6596cefa65f965f07b59530c4a01a91fc978
Gihoon Lim <knv429@naver.com>
2024-05-21 12:44:53 +0900

    동적 값 출력 및 이미지 파일 로딩

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,9 +1,19 @@
+import atomImg from "./assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={atomImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
-      <p>Fundamental React concepts you will need for almost any app you are going to build!</p>
+      <p>{description} React concepts you will need for almost any app you are going to build!</p>
     </header>
   );
 }
