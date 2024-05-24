26c2dc507178bf4117d7b096acf28e346ee00200
Salt <knv429@naver.com>
2024-05-24 23:31:46 +0900

    feat: TabButton 컴포넌트 추가

--- 02. React Essentials/src/App.jsx
+++ 02. React Essentials/src/App.jsx
@@ -1,8 +1,13 @@
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import Header from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -15,6 +20,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
