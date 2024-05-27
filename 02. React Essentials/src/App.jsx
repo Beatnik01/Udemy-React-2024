28a07e418f1a8cc5fb69ad74a4ce98fa81aac8ea
Salt <knv429@naver.com>
2024-05-27 09:16:28 +0900

    refactor: Map을 사용해 CoreConcept를 불러오도록 변경

--- 02. React Essentials/src/App.jsx
+++ 02. React Essentials/src/App.jsx
@@ -31,10 +31,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((concept) => (
+              <CoreConcept key={concept.title} {...concept} />
+            ))}
           </ul>
         </section>
         <section id="examples">
