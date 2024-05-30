b818ff3b777ecceb76232cc304b4df6e27b00485
Salt <knv429@naver.com>
2024-05-30 21:59:53 +0900

    refactor: Fragment Component → Empty tag로 변경

--- 02. React Essentials/src/App.jsx
+++ 02. React Essentials/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, Fragment } from "react";
+import { useState } from "react";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import Header from "./components/Header/Header.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
@@ -25,7 +25,7 @@ function App() {
   }
 
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
@@ -55,7 +55,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </Fragment>
+    </>
   );
 }
 
