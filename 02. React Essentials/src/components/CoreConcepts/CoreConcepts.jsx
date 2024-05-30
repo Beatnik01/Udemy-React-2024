72fd0f24458f5392183d4dc1747d7cec86626242
Salt <knv429@naver.com>
2024-05-30 22:43:21 +0900

    refactor: CoreConcepts, Examples 컴포넌트화

---
+++ 02. React Essentials/src/components/CoreConcepts/CoreConcepts.jsx
@@ -0,0 +1,15 @@
+import { CORE_CONCEPTS } from "../../data";
+import CoreConcept from "../CoreConcept/CoreConcept";
+
+export default function CoreConcepts() {
+  return (
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((concept) => (
+          <CoreConcept key={concept.title} {...concept} />
+        ))}
+      </ul>
+    </section>
+  );
+}
