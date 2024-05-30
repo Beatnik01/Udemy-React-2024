a4e7cf5199da1637290d2084acdb018b7cf7c009
Salt <knv429@naver.com>
2024-05-30 21:47:06 +0900

    refactor: div → Fragment Component로 변경

--- 02. React Essentials/src/App.jsx
+++ 02. React Essentials/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import Header from "./components/Header/Header.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
@@ -25,7 +25,7 @@ function App() {
   }
 
   return (
-    <div>
+    <Fragment>
       <Header />
       <main>
         <section id="core-concepts">
@@ -55,7 +55,7 @@ function App() {
           {tabContent}
         </section>
       </main>
-    </div>
+    </Fragment>
   );
 }
 
