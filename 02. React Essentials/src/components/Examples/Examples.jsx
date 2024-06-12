64b59e35146e99ef0f2ac1e483f3a538f8c80636
Salt <knv429@naver.com>
2024-06-12 22:32:04 +0900

    refactor: Tabs 컴포넌트화

--- 02. React Essentials/src/components/Examples/Examples.jsx
+++ 02. React Essentials/src/components/Examples/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { EXAMPLES } from "../../data";
 import TabButton from "../TabButton/TabButton";
 import Section from "../Section/Section";
+import Tabs from "../Tabs/Tabs";
 
 export default function Examples() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -25,21 +26,26 @@ export default function Examples() {
 
   return (
     <Section title="Examples" id="examples">
-      <menu>
-        <TabButton isSelected={selectedTopic === "components"} onClick={() => handleSelect("components")}>
-          Components
-        </TabButton>
-        <TabButton isSelected={selectedTopic === "jsx"} onClick={() => handleSelect("jsx")}>
-          JSX
-        </TabButton>
-        <TabButton isSelected={selectedTopic === "props"} onClick={() => handleSelect("props")}>
-          Props
-        </TabButton>
-        <TabButton isSelected={selectedTopic === "state"} onClick={() => handleSelect("state")}>
-          State
-        </TabButton>
-      </menu>
-      {tabContent}
+      <Tabs
+        buttons={
+          <>
+            <TabButton isSelected={selectedTopic === "components"} onClick={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton isSelected={selectedTopic === "jsx"} onClick={() => handleSelect("jsx")}>
+              JSX
+            </TabButton>
+            <TabButton isSelected={selectedTopic === "props"} onClick={() => handleSelect("props")}>
+              Props
+            </TabButton>
+            <TabButton isSelected={selectedTopic === "state"} onClick={() => handleSelect("state")}>
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {tabContent}
+      </Tabs>
     </Section>
   );
 }
