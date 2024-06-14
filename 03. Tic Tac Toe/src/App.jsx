7adabad65ca1c27604ef140823284e483e7a7463
Salt <knv429@naver.com>
2024-06-14 22:16:59 +0900

    feat: Player 컴포넌트 추가

--- 03. Tic Tac Toe/src/App.jsx
+++ 03. Tic Tac Toe/src/App.jsx
@@ -1,10 +1,16 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="😒" />
+          <Player name="Player 2" symbol="😂" />
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
