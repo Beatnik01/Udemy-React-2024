21a6b36156198a5b5f5fc0431a0174fb788b3998
Salt <knv429@naver.com>
2024-07-19 22:49:28 +0900

    feat: gameBoard state 및 onClick 이벤트 추가

--- 03. Tic Tac Toe/src/App.jsx
+++ 03. Tic Tac Toe/src/App.jsx
@@ -1,3 +1,4 @@
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
           <Player initialName="Player 1" symbol="😒" />
           <Player initialName="Player 2" symbol="😂" />
         </ol>
+        <GameBoard />
       </div>
     </main>
   );
