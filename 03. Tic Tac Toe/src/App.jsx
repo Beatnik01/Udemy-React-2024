52793165ef6e1ff70a474819cf4bd01a5183ae39
Salt <knv429@naver.com>
2024-07-24 01:46:50 +0900

    refactor: activePlayer state 삭제 및 유저 구분을 위한  helper function 추가

--- 03. Tic Tac Toe/src/App.jsx
+++ 03. Tic Tac Toe/src/App.jsx
@@ -3,16 +3,21 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") currentPlayer = "O";
+  return currentPlayer;
+}
+
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
   const [gameTurns, setGameTurns] = useState([]);
 
+  const activePlayer = deriveActivePlayer(gameTurns);
+
   function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") currentPlayer = "O";
-      const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: activePlayer }, ...prevTurns];
+      const currentPlayer = deriveActivePlayer(prevTurns);
+      const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
       return updatedTurns;
     });
   }
