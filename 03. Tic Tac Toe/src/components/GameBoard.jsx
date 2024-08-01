d1fa5d51354dd6731ba3fbfbf503b713bbd403de
Salt <knv429@naver.com>
2024-08-02 00:56:13 +0900

    pref: 중복 클릭 방지

--- 03. Tic Tac Toe/src/components/GameBoard.jsx
+++ 03. Tic Tac Toe/src/components/GameBoard.jsx
@@ -21,7 +21,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
